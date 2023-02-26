7c9f1936b57958d4058179c69e855f0852b353d2
javaharut <harutmargaryan91@gmail.com>
2023-02-26 19:21:02 +0400

    Improve eth price calculation for frontend

--- src/app/services/direct_contract_service.ts
+++ src/app/services/direct_contract_service.ts
@@ -1,28 +1,35 @@
 import { ethers } from 'ethers';
 
 import constants from '../config/constants';
-import aqAbi from '../abis/aqAbi';
+import aqAbi from '../abis/aq_abi';
 import provider from '../config/provider';
 
 import getEthPrice from '../utils/getEthPrice';
 
 const extractContractData = async (wallet_address: string) => {
-  const aq1_contract = new ethers.Contract(constants.AQ_CONTRACT, aqAbi, provider);
+  const aq1_contract = new ethers.Contract(
+    constants.AQ_CONTRACT,
+    aqAbi,
+    provider
+  );
 
   const tokenCount = await aq1_contract.balanceOf(wallet_address);
   const treasuryStake = (Number(tokenCount) / constants.TOTAL_BALANCE) * 100;
 
-  const treasuryBalance = await provider.getBalance(constants.ROYALTY_RECIPIENT_ADDRESS);
-  const ethPriceInUSD = await getEthPrice(provider);
+  const treasuryBalance = await provider.getBalance(
+    constants.ROYALTY_RECIPIENT_ADDRESS
+  );
+  const { value, decimals } = await getEthPrice(provider);
 
-  const treasuryAssetValue = treasuryBalance * ethPriceInUSD;
+  const treasuryAssetValue = (treasuryBalance * value) / 10n ** decimals;
+  const ethPriceInUSD = Number((value * 100n) / 10n ** decimals) / 100;
 
   return {
     treasuryStake,
-    ethPriceInUSD: Number(ethPriceInUSD),
+    ethPriceInUSD,
     treasuryBalance: ethers.formatEther(treasuryBalance),
     treasuryAssetValue: ethers.formatEther(treasuryAssetValue),
-  }
+  };
 };
 
 export default extractContractData;
